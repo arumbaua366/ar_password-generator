4afd0defeaa0f730c6c64989de208ee1be35ff7c
ar <56378858+arumbaua366@users.noreply.github.com>
2020-01-18 23:17:23 -0800

    updated js code

--- js/script.js
+++ js/script.js
@@ -10,42 +10,43 @@
 // set variables 
 
 // these are being pulled from HTML using querySelector for their IDs.
-var charRange = document.querySelector("#pwRange")
-var charLength = document.querySelector("#pwLength")
-var symCheck = document.querySelector("#defaultCheck1")
-var numCheck = document.querySelector("#defaultCheck2")
-var passDisplay = document.querySelector("#formGroupExampleInput")
-var form = document.querySelector("#pwGeneratorForm")
+var charRange = document.querySelector("#pwRange");
+var charLength = document.querySelector("#pwLength");
+var symCheck = document.querySelector("#defaultCheck1");
+var numCheck = document.querySelector("#defaultCheck2");
+var passDisplay = document.querySelector("#formGroupExampleInput");
+var form = document.querySelector("#pwGeneratorForm");
 
 // used character codes to pull all characters instead of using an array or a string.
-var alphChars = arrayFromLowtoHigh(65, 90).concat(97, 122)
-var numChars = arrayFromLowtoHigh(48, 57)
-var symChars = arrayFromLowtoHigh(33, 47).concat(58, 64).concat(91, 96)
+var alphChars = arrayFromLowtoHigh(65, 90).concat(97, 122);
+var numChars = arrayFromLowtoHigh(48, 57);
+var symChars = arrayFromLowtoHigh(33, 47).concat(58, 64).concat(91, 96);
 
 // eventListener is used for the slider and pw length input to call on the same function (charLengthActivate).
-charRange.addEventListener("input", charLengthActivate)
-charLength.addEventListener("input", charLengthActivate)
+charRange.addEventListener("input", charLengthActivate);
+charLength.addEventListener("input", charLengthActivate);
 
 form.addEventListener("submit", event => {
-    event.preventDefault()
-    var charAmount = charLength.value
-    var addSym = symCheck.checked
-    var addNum = numCheck.checked
-    var password = genPass(charAmount, addSym, addNum)
-    passDisplay.innerText = password
-});
+    event.preventDefault();
+    var charAmount = charLength.value;
+    var addSym = symCheck.checked;
+    var addNum = numCheck.checked;
+    var password = genPass(charAmount, addSym, addNum);
+    passDisplay.innerText = password;
+})
 
+// function that generates the random passwords
 function genPass(charAmount, addSym, addNum){
-   var charCodes = alphChars
+   var charCodes = alphChars;
    if (addSym) charCodes = charCodes.concat(symChars); // adds on symbols to random password
    if (addNum) charCodes = charCodes.concat(numChars); // adds on numbers to random password
    
    var passChars = []
    for (var i = 0; i < charAmount; i++){
-    var charMaster = charCodes[Math.floor(Math.random() * charCodes.length)]
-    passChars.push(String.fromCharCode(charMaster))
-   }
-   return passChars.join("") // returns array into a string
+    var charMaster = charCodes[Math.floor(Math.random() * charCodes.length)];
+    passChars.push(String.fromCharCode(charMaster));
+   };
+   return passChars.join(""); // returns array into a string
 }
 
 function arrayFromLowtoHigh(low, high){
@@ -62,3 +63,12 @@ function charLengthActivate(event){
     charLength.value = value
 }
 
+// copies password to clipboard
+function clipboardCopy(){
+    var copyText = document.querySelector("#formGroupExampleInput")
+    copyText.querySelector();
+    document.execCommand("copy")
+    alert("Copied to Clipboard!")
+}
+
+document.querySelector("#clipboard").addEventListener("click", copy);
